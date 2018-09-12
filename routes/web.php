8e969a904a8539604116a80847bf66aadfd61929
amanation <fractalemanation@mail.ru>
2018-09-12 20:53:32 +0300

    reload

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 
 Route::get('/', function () {
-	App\Jobs\SendMessage::withChain([
+	/*App\Jobs\SendMessage::withChain([
 		new App\Jobs\PrepareJob('Prepare...'),
 		new App\Jobs\PublishJob('Publish!')
-	])->dispatch("Start Job");//withChain - разбивает на массив и если не выполняется первое условие, то не выполняется и следующие
+	])->dispatch("Start Job");*///withChain - разбивает на массив и если не выполняется первое условие, то не выполняется и следующие
 	//App\Jobs\SendMessage::dispatch("Test message")->delay(now()->addMinutes(10));//Передает данные в конструктор с помощью dispatch; delay установили с какого момента можно выполнять задание
-    //return view('default');
+    return view('default');
 });
 
 Route::post('/image/upload', 'ImageController@upload')->name('image.upload');
